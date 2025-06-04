6b296e18ae4e4693ea080c66457c6a4f79aa362a
Ionescu Liviu Cristian <liviu@purplecat-labs.com>
2025-06-04 15:55:44 +0300

    Add .bashrc

--- install_aliases.sh
+++ install_aliases.sh
@@ -5,6 +5,7 @@ set -eu
 # Install bash aliases
 # ==========================================
 
+cp ./dotfiles/.bashrc ~/.bashrc
 cp ./dotfiles/.bash_aliases ~/.bash_aliases
 cp ./dotfiles/.bash_aliases_help ~/.bash_aliases_help
 cp ./dotfiles/.bash_aliases_private ~/.bash_aliases_private
