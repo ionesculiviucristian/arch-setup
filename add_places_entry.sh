3f5743ad4aa43f7b6a06bfa5108fbd9ba594e2b9
Ionescu Liviu Cristian <ionescu.liviucristian@yahoo.com>
2025-06-14 18:29:57 +0300

    Chores

--- add_places_entry.sh
+++ add_places_entry.sh
@@ -8,4 +8,16 @@ PLACES_FILE=~/.local/share/user-places.xbel
 
 [ ! -f "${PLACES_FILE}" ] && echo '<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE xbel><xbel version="1.0"></xbel>' > "${PLACES_FILE}"
 
-sed -i "/<\/xbel>/i <bookmark href=\"file://${PLACE_PATH}\"><title>${PLACE_NAME}</title><info><metadata owner=\"http://freedesktop.org\"><bookmark:icon name=\"folder\"/></metadata><metadata owner=\"http://www.kde.org\"><ID>${UNIQUE_ID}</ID><isSystemItem>false</isSystemItem></metadata></info></bookmark>" "${PLACES_FILE}"
+sed -i "/<\/xbel>/i\\
+ <bookmark href=\"file://${PLACE_PATH}\">\\
+  <title>${PLACE_NAME}</title>\\
+  <info>\\
+   <metadata owner=\"http://freedesktop.org\">\\
+    <bookmark:icon name=\"folder\"/>\\
+   </metadata>\\
+   <metadata owner=\"http://www.kde.org\">\\
+    <ID>${UNIQUE_ID}</ID>\\
+    <isSystemItem>false</isSystemItem>\\
+   </metadata>\\
+  </info>\\
+ </bookmark>" "${PLACES_FILE}"
