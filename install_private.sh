7be4199098d5e70308cb27cde62940513b4c7d6b
Ionescu Liviu Cristian <ionescu.liviucristian@yahoo.com>
2025-07-17 20:42:52 +0300

    WIP

--- install_private.sh
+++ install_private.sh
@@ -15,7 +15,7 @@ BW_SESSION=$(bw login "${email}" --method 0 --raw)
 export BW_SESSION
 
 bw get item "Private bash aliases" | jq -r '.notes' > "${HOME}/.bash_private_aliases"
-bw get item "Arch setup" | jq -r '.notes' > \.env
+bw get item "Arch setup" | jq -r '.notes' > "./env"
 bw get item "GPG private key" | jq -r '.notes' | gpg --import
 bw get item "GPG public key" | jq -r '.notes' | gpg --import
 
