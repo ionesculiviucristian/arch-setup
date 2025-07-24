47e8fc45e157191d6f814c1b7c13092c976f53df
Ionescu Liviu Cristian <ionescu.liviucristian@yahoo.com>
2025-07-24 12:16:15 +0300

    WIP

--- setups/base/sddm.sh
+++ setups/base/sddm.sh
@@ -15,15 +15,20 @@ sudo pacman \
   qt6-declarative \
   qt5-quickcontrols2
 
-wget -qO- https://github.com/catppuccin/sddm/releases/download/v1.1.0/catppuccin-mocha.zip | sudo bsdtar -xvf- -C "${sddm_themes_dir}"
+sudo rm -rf "${sddm_themes_dir}/catppuccin-mocha-mauve-sddm"
+sudo mkdir -p "${sddm_themes_dir}/catppuccin-mocha-mauve-sddm"
+
+wget -qO- \
+  https://github.com/catppuccin/sddm/releases/download/v1.1.1/catppuccin-mocha-mauve-sddm.zip | \
+  sudo bsdtar -xvf- -C "${sddm_themes_dir}/catppuccin-mocha-mauve-sddm"
 
 sudo sed -i \
   -e 's/^CustomBackground="false"/CustomBackground="true"/' \
-  "${sddm_themes_dir}/catppuccin-mocha/theme.conf"
+  "${sddm_themes_dir}/catppuccin-mocha-mauve-sddm/theme.conf"
 
 sudo cp \
   "${wallpapers_dir}/wp11361931-minimalist-3440x1440-wallpapers.png" \
-  "${sddm_themes_dir}/catppuccin-mocha/backgrounds/wall.jpg"
+  "${sddm_themes_dir}/catppuccin-mocha-mauve-sddm/backgrounds/wall.png"
 
 sudo cp \
   "./configs/etc/sddm.conf.d/kde_settings.conf" \
