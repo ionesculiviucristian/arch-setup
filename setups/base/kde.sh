6eecd0bd9fd8a80ff92607d1949c125f107a0bd4
Ionescu Liviu Cristian <ionescu.liviucristian@yahoo.com>
2025-07-26 21:04:35 +0300

    WIP

--- setups/base/kde.sh
+++ setups/base/kde.sh
@@ -9,21 +9,21 @@ source "./scripts/helpers.sh" 2
 catppuccin_kde_dir="${HOME}/.repos/catppuccin-fzf"
 icons_dir="${HOME}/.local/share/icons"
 
-_rmdir"${catppuccin_kde_dir}"
+_rmdir "${catppuccin_kde_dir}"
 
 git clone -q --depth=1 https://github.com/catppuccin/kde "${catppuccin_kde_dir}"
 
-_rmdir"${HOME}/.local/share/kpackage/generic/Catppuccin-Mocha-Mauve"
+_rmdir "${HOME}/.local/share/kpackage/generic/Catppuccin-Mocha-Mauve"
 
 (
   cd "${catppuccin_kde_dir}"
   printf "1\n4\n2\ny\ny" | ./install.sh 2>&1 | tee -a "./install.log" >/dev/null
 
-  _rmdir"${icons_dir}/Catppuccin-Mocha-Dark-Cursors"
-  _rmdir"${icons_dir}/Catppuccin-Mocha-Mauve-Cursors"
+  _rmdir "${icons_dir}/Catppuccin-Mocha-Dark-Cursors"
+  _rmdir "${icons_dir}/Catppuccin-Mocha-Mauve-Cursors"
 
   # Use newer Catppuccin cursors
-  _rmdir"${icons_dir}/icons/catppuccin-mocha-mauve-cursors"
+  _rmdir "${icons_dir}/icons/catppuccin-mocha-mauve-cursors"
   wget -qO- https://github.com/catppuccin/cursors/releases/download/v2.0.0/catppuccin-mocha-mauve-cursors.zip | bsdtar -xvf- -C "${icons_dir}" 2>&1 | tee -a "./install.log" >/dev/null
   
   if [ ! -L "${HOME}/.icons" ]; then
