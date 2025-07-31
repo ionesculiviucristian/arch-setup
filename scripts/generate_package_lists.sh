caa6c221823724ab7c78c808dfca52a98f0fb5d3
Ionescu Liviu Cristian <ionescu.liviucristian@yahoo.com>
2025-07-31 23:00:57 +0300

    WIP

--- scripts/generate_package_lists.sh
+++ scripts/generate_package_lists.sh
@@ -8,14 +8,15 @@ if [ ! -f "${data_file}" ]; then
   exit 1
 fi
 
-jq -r 'keys_unsorted[] | select(. != "external")' "${data_file}" | while read -r list; do
-  list_file="./data/lists/${list}.txt"
+jq -r 'keys_unsorted[] | select(. != "external")' "${data_file}" | \
+  while read -r list; do
+    list_file="./data/lists/${list}.txt"
 
-  jq --arg section_name "${list}" -r '
-    .[$section_name]
-    | sort_by(.name)
-    | .[] | .name
-  ' "${data_file}" > "${list_file}"
-done
+    jq --arg section_name "${list}" -r '
+      .[$section_name]
+      | sort_by(.name)
+      | .[] | .name
+    ' "${data_file}" > "${list_file}"
+  done
 
 exit 0
