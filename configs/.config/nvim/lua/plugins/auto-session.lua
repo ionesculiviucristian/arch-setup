85f49dec49f1e6a1f58e4987c15a64a7b1c8a141
Ionescu Liviu Cristian <ionescu.liviucristian@yahoo.com>
2025-08-20 10:59:18 +0300

    feat: add auto-session to nvim

---
+++ configs/.config/nvim/lua/plugins/auto-session.lua
@@ -0,0 +1,9 @@
+-- https://github.com/rmagatti/auto-session?tab=readme-ov-file#-installation
+return {
+  "rmagatti/auto-session",
+  lazy = false,
+  -- https://github.com/rmagatti/auto-session?tab=readme-ov-file#%EF%B8%8F-configuration
+  opts = {
+    suppressed_dirs = { "~/", "~/Downloads", "/" },
+  },
+}
