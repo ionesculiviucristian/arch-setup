eb53b2e80dc32d8283cd3091c2891df8d3cd0fe9
Ionescu Liviu Cristian <ionescu.liviucristian@yahoo.com>
2025-10-29 18:23:23 +0200

    feat: use tpm as plugin manager for tmux

--- setups/base/official/tmux.sh
+++ setups/base/official/tmux.sh
@@ -1,28 +1,16 @@
 #!/bin/bash
 set -eu
 
-# [Theme] https://github.com/catppuccin/tmux
+tmux_plugins_dir="${HOME}/.tmux/plugins"
 
-tmux_plugins_dir="${HOME}/.config/tmux/plugins"
+rm -rf "${tmux_plugins_dir}"
 
-mkdir -p "${tmux_plugins_dir}/tmux-plugins"
+mkdir -p "${tmux_plugins_dir}"
 
-rm -rf "${tmux_plugins_dir}/tmux-plugins/tmux-cpu"
-rm -rf "${tmux_plugins_dir}/tmux-plugins/tmux-battery"
-rm -rf "${tmux_plugins_dir}/catppuccin"
-
-git clone -q \
-  https://github.com/tmux-plugins/tmux-cpu \
-  "${tmux_plugins_dir}/tmux-plugins/tmux-cpu"
-
-git clone -q \
-  https://github.com/tmux-plugins/tmux-battery \
-  "${tmux_plugins_dir}/tmux-plugins/tmux-battery"
-
-git clone -q \
-  https://github.com/catppuccin/tmux.git \
-  "${tmux_plugins_dir}/catppuccin/tmux"
+git clone https://github.com/tmux-plugins/tpm "${tmux_plugins_dir}/tpm"
 
 cp "./configs/.tmux.conf" "${HOME}/.tmux.conf"
+
+"${HOME}/.tmux/plugins/tpm/bin/install_plugins"
 
 exit 0
