ee0c18a0d39a7e86b25fcf08b5a72e1556c63d49
Ionescu Liviu Cristian <ionescu.liviucristian@yahoo.com>
2025-12-03 17:13:29 +0200

    fix: use correct divider

--- setups/base/system.sh
+++ setups/base/system.sh
@@ -10,7 +10,8 @@ sudo sed -i '/\[multilib\]/,/Include =/ s/^#//' "/etc/pacman.conf"
 sudo_replace_text \
   '^#precedence ::ffff:0:0/96  100' \
   'precedence ::ffff:0:0/96  100' \
-  "/etc/gai.conf"
+  "/etc/gai.conf" \
+  "|"
 
 cp "./configs/powerdevilrc" "${HOME}/.config/powerdevilrc"
 
